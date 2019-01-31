ed39458dea6543266fde5451d641e15311ceead6
Unknown <sinclair.angus@gmail.com>
2019-01-31 09:58:45 -0800

    Added get_hourly code

--- aws_lambda.py
+++ aws_lambda.py
@@ -2,6 +2,19 @@ import numpy as np
 from scipy.interpolate import UnivariateSpline
 from sklearn.base import BaseEstimator, RegressorMixin
 from sklearn.linear_model import LinearRegression
+import pandas as pd
+from dateutil import parser
+import dask.dataframe as dd
+import gc
+import os
+import boto3
+
+
+# AWS
+AWS_ACCESS_KEY_ID = os.environ.get["AWS_ACCESS_KEY_ID"]
+AWS_SECRET_ACCESS_KEY = os.environ.get["AWS_SECRET_ACCESS_KEY"]
+s3 = boto3.resource('s3')
+bucket = s3.Bucket('whisky-pricing')
 
 
 class OutlierLinearRegression(BaseEstimator, RegressorMixin):
@@ -62,13 +75,44 @@ class SplineRegressor(BaseEstimator, RegressorMixin):
 
 def get_hourly():
     # Load up full data
+    tzinfos = {'BST': 3600,
+               'GMT': 0}
+
+    df = dd.read_csv('days/*', parse_dates=False)
+    unique_time = df.time.unique()
+    fix_time = unique_time.map(lambda x: pd.Timestamp(parser.parse(x, tzinfos=tzinfos)) \
+                               .tz_convert('UTC'), meta=pd.Series([], dtype='datetime64[ns, UTC]', name='fix_time'))
+
     # Regroup to hourly
+    fix_time = fix_time.dt.floor('h')  # Get hour only
+    unique_time = dd.concat([unique_time, fix_time], axis=1, )
+    unique_time.columns = ['time', 'fixed_time']
+    unique_time = unique_time.groupby('fixed_time').first().reset_index()
+    df = df.merge(unique_time, left_on='time', right_on='time', how='inner')
+    df = df.compute()
+
     # Calculate spreads
-    # Dump results to csv
-    # run_regression
-    # Regroup to daily
-    # Save daily to csv
-    return
+    max_buy = df[df['buy'] == 1].groupby(['pitchId', 'fixed_time'])[['limit']].max()
+    min_sell = df[df['buy'] == 0].groupby(['pitchId', 'fixed_time']).limit.min()
+    spreads = max_buy.join(min_sell, on=['pitchId', 'fixed_time'], how='outer', rsuffix='r')
+    spreads.columns = ['max_buy', 'min_sell']
+    spreads = spreads.reset_index()
+    spreads.columns = ['pitchId', 'time', 'max_buy', 'min_sell']
+    spreads.set_index('time', inplace=True)
+
+    # Free some RAM
+    del max_buy, min_sell, df
+    gc.collect()
+
+    # Dump results to csv - this need to be on aws using boto3
+    spreads.to_csv('spreads.csv')
+    bucket.upload_file('spreads.csv', 'spreads.csv')
+
+    # Regroup to daily # Save daily to csv
+    spreads.groupby('pitchId').resample('D')[['min_sell', 'max_buy']].mean().to_csv('mean_daily_spread.csv')
+    bucket.upload_file('mean_daily_spread.csv', 'mean_daily_spread.csv')
+
+    return spreads
 
 
 def run_regression(df):
@@ -97,6 +141,14 @@ def run_regression(df):
     pitches['fee_adjusted_purchase_cost'] = np.round(pitches.min_sell * (1 + market_fee), 2)
     pitches['annual_return'] = 100 * 365.25 * pitches.adjusted_slope / pitches.fee_adjusted_purchase_cost
 
+    # Save csv
+    pitches.to_csv('pitch_models.csv')
+    bucket.upload_file('pitch_models.csv', 'pitch_models.csv')
+
     return pitches
 
+
 if __name__ == '__main__':
+    df = get_hourly()
+    pitches = run_regression(df)
+    pitches.head()
