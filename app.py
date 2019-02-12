c2fdf30d4095187ef0f12a617f4c926ae2b5b9b8
Unknown <sinclair.angus@gmail.com>
2019-02-12 08:41:00 -0800

    Remove import dash datatable

--- app.py
+++ app.py
@@ -3,7 +3,6 @@ import dash
 import dash_core_components as dcc
 import dash_html_components as html
 import dash_bootstrap_components as dbc
-from dash_table import DataTable
 from dash.dependencies import Input, Output
 from dash_html_template import Template
 import os
