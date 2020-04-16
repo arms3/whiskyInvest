91f0099fb6c1414e4178b5cc70c5dcfb9336ff62
slalom-angus <55515670+slalom-angus@users.noreply.github.com>
2020-04-16 10:04:11 -0700

    Updated Lambda script to import the delta only

--- aws/awsLambda.py
+++ aws/awsLambda.py
@@ -1,7 +1,6 @@
 #!/usr/bin/env python
 import gc
-import boto3
-import dask.dataframe as dd
+import s3fs
 import numpy as np
 import pandas as pd
 from dateutil import parser
@@ -9,6 +8,8 @@ from scipy.interpolate import UnivariateSpline
 from sklearn.base import BaseEstimator, RegressorMixin
 from sklearn.linear_model import LinearRegression
 
+# Initialize S3 connection
+s3 = s3fs.S3FileSystem(anon=False)
 
 class OutlierLinearRegression(BaseEstimator, RegressorMixin):
     def __init__(self, outlierpct, reg=LinearRegression()):
@@ -66,44 +67,6 @@ class SplineRegressor(BaseEstimator, RegressorMixin):
         return np.concatenate([self.spl(X[in_train]), self.future(X[~in_train])])
 
 
-def get_hourly():
-    # Load up full data
-    print('Loading hourly data...')
-    tzinfos = {'BST': 3600,
-               'GMT': 0}
-
-    df = dd.read_csv('s3://whisky-pricing/20*.csv', parse_dates=False)
-    # debug local
-    # df = dd.read_csv(r'C:\\Users\\sincl\\OneDrive\\Desktop\\days\\2018-12-2*.csv', parse_dates=False)
-    unique_time = df.time.unique()
-    fix_time = unique_time.map(lambda x: pd.Timestamp(parser.parse(x, tzinfos=tzinfos))
-                               .tz_convert('UTC'), meta=pd.Series([], dtype='datetime64[ns, UTC]', name='fix_time'))
-
-    # Regroup to hourly
-    print('Regrouping to hourly...')
-    fix_time = fix_time.dt.floor('h')  # Get hour only
-    unique_time = dd.concat([unique_time, fix_time], axis=1, )
-    unique_time.columns = ['time', 'fixed_time']
-    unique_time = unique_time.groupby('fixed_time').first().reset_index()
-    df = df.merge(unique_time, left_on='time', right_on='time', how='inner')
-    df = df.compute()
-    df.drop('time', axis=1, inplace=True)
-    df.rename({'fixed_time': 'time'}, axis=1, inplace=True)
-
-    # Calculate spreads
-    print('Calculating spreads...')
-    max_buy = df[df['buy'] == 1].groupby(['pitchId', 'time'])[['limit']].max()
-    min_sell = df[df['buy'] == 0].groupby(['pitchId', 'time'])[['limit']].min()
-    del df
-    gc.collect()
-    spreads = max_buy.join(min_sell, on=['pitchId', 'time'], how='outer', rsuffix='r')
-    spreads.columns = ['max_buy', 'min_sell']
-    spreads = spreads.reset_index()
-    spreads.columns = ['pitchId', 'time', 'max_buy', 'min_sell']
-    spreads.set_index('time', inplace=True)
-    return spreads
-
-
 def index2int(index):
     """Helper function to convert timeindexes to ints"""
     return index.astype(np.int64).values.reshape(-1,1)
@@ -112,6 +75,69 @@ def index2int(index):
 def int2dt(array):
     """Helper function to convert int64 to timeseries[ns]"""
     return pd.to_datetime(pd.Series(array.squeeze(), name='timestamp'),utc=True)
+
+
+def regroup_df_hourly(df, time_column='time'):
+    # Get list of unique timestamps
+    unique_time = df.time.unique()
+
+    # Ensure we have UTC aware timestamps
+    tzinfos = {'BST': 3600, 'GMT': 0}
+    fix_time = pd.Series([pd.Timestamp(parser.parse(x, tzinfos=tzinfos)).tz_convert('UTC') for x in unique_time],dtype='datetime64[ns, UTC]', name='fix_time')
+
+    fix_time = fix_time.dt.floor('h')  # Get hour only
+    unique_time = pd.concat([pd.Series(unique_time, name=time_column), fix_time], axis=1)
+    unique_time = unique_time.groupby('fix_time').first().reset_index()
+    df = df.merge(unique_time, left_on=time_column, right_on=time_column, how='inner') # Drop all timestamps not on the hour
+    df.drop(time_column, axis=1, inplace=True)
+    df.rename({'fix_time': time_column}, axis=1, inplace=True)
+    return df
+
+
+def get_hourly():
+    # Load up full data
+    print("Reading existing hourly spread data...")
+    df = pd.read_csv('s3://whisky-pricing/spreads.csv', parse_dates=['time'])
+   
+   # Get latest imported date
+    print("Importing non consolidated dates...")
+    last_date = pd.to_datetime(df.time.max()).date()
+    today = pd.to_datetime('today').date()
+
+    # Get missing days
+    missing_days = []
+    delta = today - last_date
+    for i in range(delta.days):
+        day = last_date + pd.Timedelta(i+1, unit='D')
+        try:
+            df_day = pd.read_csv(f's3://whisky-pricing/{day}.csv', parse_dates=False)
+            print(f'Imported day: {day}')
+            missing_days.append(df_day)
+        except:
+            print(f'Day not found: {day}')
+
+    if len(missing_days) == 0:
+        return df, False # Return flag so that further processing can be skipped
+    
+    # Concat all the missing days
+    missing = pd.concat(missing_days,axis=0)
+
+    # Regroup to hourly
+    missing = regroup_df_hourly(missing, time_column='time')
+
+    # Calculate spreads
+    print('Calculating spreads...')
+    max_buy = missing[missing['buy'] == 1].groupby(['pitchId', 'time'])[['limit']].max()
+    min_sell = missing[missing['buy'] == 0].groupby(['pitchId', 'time'])[['limit']].min()
+    missing = max_buy.join(min_sell, on=['pitchId', 'time'], how='outer', rsuffix='r')
+    missing.columns = ['max_buy', 'min_sell']
+    missing = missing.reset_index()
+    missing.columns = ['pitchId', 'time', 'max_buy', 'min_sell']
+
+    # Merge with existing data
+    df = pd.concat([df,missing],axis=0)
+    df.drop('predict',axis=1, inplace=True)
+    return df, True # Return flag to continue processing
 
 
 def run_regression(df):
@@ -130,19 +156,20 @@ def run_regression(df):
 
     linreg = pd.DataFrame.from_dict(linreg, orient='index')
 
-    # Combine preds append to df and save
-    print('Combining daily predictions and uploading...')
+    # Combine preds append to df
+    print('Combining daily predictions...')
     preds = pd.concat(preds, axis=0).reset_index()
     df = df.reset_index().merge(preds, on=['pitchId','time']).reset_index().set_index('time')
     df.drop('index',axis=1, inplace=True) # Remove the rangindex artifact created in merge
-    df.to_csv('spreads.csv')
-    bucket.upload_file('spreads.csv', 'spreads.csv')
+    return df, linreg
 
-    # Regroup to daily # Save daily to csv
-    print('Uploading daily pricing...')
-    df.groupby('pitchId').resample('D')[['min_sell', 'max_buy', 'predict']].mean().to_csv('mean_daily_spread.csv')
-    bucket.upload_file('mean_daily_spread.csv', 'mean_daily_spread.csv')
+def regroup_to_daily(df):
+    # Regroup to daily
+    print('Regrouping daily pricing...')
+    df.groupby('pitchId').resample('D')[['min_sell', 'max_buy', 'predict']].mean()
+    return df
 
+def calculate_returns(df, linreg):
     # Fees
     market_fee = 1.75 / 100.0  # % per transaction, rounded to nearest pence
     storage_fee = 0.15  # pence per annum (charged monthly)
@@ -162,19 +189,25 @@ def run_regression(df):
                              + (pitches.adjusted_slope * 365.25)  # Use model price here
     pitches['fee_adjusted_sell_cost'] = pitches.predicted1y * (1 - market_fee)
     pitches['annual_return'] = -100 + 100 * pitches.fee_adjusted_sell_cost / pitches.fee_adjusted_purchase_cost
-
-    # Save csv
-    print('Uploading model...')
-    pitches.to_csv('pitch_models.csv')
-    bucket.upload_file('pitch_models.csv', 'pitch_models.csv')
-
     return pitches
 
 
 if __name__ == '__main__':
     print('Reticulating splines...')
-    s3 = boto3.resource('s3')
-    bucket = s3.Bucket('whisky-pricing')
-    df = get_hourly()
-    pitches = run_regression(df)
-    print(pitches.head(3))
+    hourly, continueProcessing = get_hourly()
+    if continueProcessing:
+        # Process hourly predictions and upload to S3
+        hourly_pred, linreg = run_regression(hourly)
+        hourly_pred.to_csv('spreads.csv')
+        s3.put('spreads.csv','s3://whisky-pricing/spreads.csv')
+        
+        # Process daily data and upload to S3
+        daily = regroup_to_daily(hourly_pred)
+        daily.to_csv('mean_daily_spread.csv')
+        s3.put('mean_daily_spread.csv', 's3://whisky-pricing/mean_daily_spread.csv')
+
+        # Caluclate returns and upload to S3
+        pitches = calculate_returns(daily, linreg)
+        pitches.to_csv('pitch_models.csv')
+        s3.put('pitch_models.csv', 's3://whisky-pricing/pitch_models.csv')
+        print(pitches.head(3))
